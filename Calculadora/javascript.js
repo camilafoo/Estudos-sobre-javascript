8a75cd5feca1cc4ab3783174059d73403ec0ce50
mariacamilaa1107 <mariacamilaa1107@gmail.com>
2018-08-29 20:26:21 -0300

    Calculadora

---
+++ Calculadora/javascript.js
@@ -0,0 +1,34 @@
+function somar() {
+    var campo1 = parseInt(document.getElementById("campo1").value);
+    var campo2 = parseInt(document.getElementById("campo2").value);
+
+   var soma = campo1 + campo2;
+
+   alert(soma);
+}
+
+function subtração() {
+    var campo3 = parseInt(document.getElementById("campo3").value);
+    var campo4 = parseInt(document.getElementById("campo4").value);
+
+   var subtração = campo3 - campo4;
+
+   alert(subtração);
+}
+function multiplicar() {
+    var campo5 = parseInt(document.getElementById("campo5").value);
+    var campo6 = parseInt(document.getElementById("campo6").value);
+
+   var multiplicar = campo5 * campo6;
+
+   alert(multiplicar);
+}
+
+function divisão() {
+    var campo7 = parseInt(document.getElementById("campo7").value);
+    var campo8 = parseInt(document.getElementById("campo8").value);
+
+   var divisão = campo7 / campo8;
+
+   alert(divisão);
+}
