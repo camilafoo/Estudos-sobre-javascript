5522fa3afd55ab1072d86cb1c680db7e55cc895a
camila oliveira <mariacamilaa1107@gmail.com>
2018-09-20 17:40:46 -0300

    Estudos

---
+++ Mudar imagem/js/js.js
@@ -0,0 +1,7 @@
+function trocarImg (){
+    document.getElementById('natu').src='natureza2.jpg';
+    
+}
+function voltarImg (){
+ document.getElementById('natu').src="natureza1.jpg";
+}
---
+++ lampada/js/js.js
@@ -0,0 +1,9 @@
+function trocarImg (){
+    document.getElementById('lamp').src='lamp_on.jpg';
+}
+function voltarImg(){
+    document.getElementById('lamp').src='lamp_off.jpg';
+}
+function quebrarImg(){
+    document.getElementById('lamp').src='lamp_quebrada.jpg';
+}
