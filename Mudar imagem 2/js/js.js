539b066634ebf701ee607d00f646a5904789a271
camila oliveira <mariacamilaa1107@gmail.com>
2018-09-20 18:34:25 -0300

    Em andamento

---
+++ Mudar imagem 2/js/js.js
@@ -0,0 +1,40 @@
+function muda1 (){
+	document.getElementById('troca').src='img1.jpg';
+
+}
+function muda2 (){
+	document.getElementById('troca').src='img2.jpg';
+	
+}
+function muda3 (){
+	document.getElementById('troca').src='img3.jpg';
+	
+}
+function muda4 (){
+	document.getElementById('troca').src='img4.jpg';
+	
+}
+function muda5 (){
+	document.getElementById('troca').src='img5.jpg';
+	
+}
+function muda6 (){
+	document.getElementById('troca').src='img6.jpg';
+	
+}
+function muda7 (){
+	document.getElementById('troca').src='img7.jpg';
+	
+}
+function muda8 (){
+	document.getElementById('troca').src='img8.jpg';
+	
+}
+function muda9 (){
+	document.getElementById('troca').src='img9.jpg';
+	
+}
+function muda10 (){
+	document.getElementById('troca').src='img10.jpg';
+	
+}
