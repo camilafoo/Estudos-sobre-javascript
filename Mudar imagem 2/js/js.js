3bcf224e414962b3bd83f995c40fb804719b31a8
camila oliveira <mariacamilaa1107@gmail.com>
2018-09-20 19:10:39 -0300

    Projeto finalizado

--- Mudar imagem 2/js/js.js
+++ Mudar imagem 2/js/js.js
@@ -7,34 +7,34 @@ function muda2 (){
 	
 }
 function muda3 (){
-	document.getElementById('troca').src='img3.jpg';
+	document.getElementById('troca').src='img3n.jpg';
 	
 }
 function muda4 (){
-	document.getElementById('troca').src='img4.jpg';
+	document.getElementById('troca').src='img4n.png';
 	
 }
 function muda5 (){
-	document.getElementById('troca').src='img5.jpg';
+	document.getElementById('troca').src='img5n.png';
 	
 }
 function muda6 (){
-	document.getElementById('troca').src='img6.jpg';
+	document.getElementById('troca').src='img6n.jpg';
 	
 }
 function muda7 (){
-	document.getElementById('troca').src='img7.jpg';
+	document.getElementById('troca').src='img7n.jpg';
 	
 }
 function muda8 (){
-	document.getElementById('troca').src='img8.jpg';
+	document.getElementById('troca').src='img8n.png';
 	
 }
 function muda9 (){
-	document.getElementById('troca').src='img9.jpg';
+	document.getElementById('troca').src='img9n.jpg';
 	
 }
 function muda10 (){
-	document.getElementById('troca').src='img10.jpg';
+	document.getElementById('troca').src='img10n.jpg';
 	
 }
