a83eaa9219b5d77c71a8d39132f60597b8473b5e
camila oliveira <mariacamilaa1107@gmail.com>
2018-09-24 16:58:34 -0300

    Trabalho Pronto

--- lampada/js/js.js
+++ lampada/js/js.js
@@ -1,9 +1,9 @@
 function trocarImg (){
-    document.getElementById('lamp').src='lamp_on.jpg';
+    document.getElementById('lamp').src='lamp_on.png';
 }
 function voltarImg(){
-    document.getElementById('lamp').src='lamp_off.jpg';
+    document.getElementById('lamp').src='lamp_off.png';
 }
 function quebrarImg(){
-    document.getElementById('lamp').src='lamp_quebrada.jpg';
+    document.getElementById('lamp').src='lamp_quebrada.png';
 }
