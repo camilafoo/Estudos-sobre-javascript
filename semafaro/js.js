a8d17897a77b562baf6a421106b5b0f1d8fe3578
Camila Oliveira <mariacamilaa1107@gmail.com>
2018-10-23 15:50:23 -0200

    mundança no estilo

---
+++ semafaro/js.js
@@ -0,0 +1,16 @@
+function verde(){
+    if(onclick=verde){
+        document.write("Sigaaaaaaa!!!!!!");
+    }
+}
+function amarelo(){
+    if(onclick=amarelo){
+        document.write("Atençãoooooo!!!!!!");
+    }
+}
+function vermelho(){
+    if(onclick=vermelho){
+        document.write("Pare!!!!!!!!");
+    }
+}
+
