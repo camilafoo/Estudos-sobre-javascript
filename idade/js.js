2e81fde72adcce7509cc8d78ba85a65bb9a248a2
Camila Oliveira <mariacamilaa1107@gmail.com>
2018-10-30 17:46:01 -0300

    Alterações

--- idade/js.js
+++ idade/js.js
@@ -2,9 +2,10 @@ function validar (){
     var nome= formuser.nome.value;
     var idade= formuser.idade.value;
     if(idade>=18){
-        document.write('Nome: ' + nome + "<br>" + "Idade: " + idade + "<br>" + "Você é maior de idade.");
+      
+        document.getElementById('resultado').textContent="Você é maior de idade";
     }else{
-        document.write('Nome: ' + nome + "<br>" + "Idade: " + idade + "<br>" + "Você é menor de idade.");
+        document.getElementById("resultado").textContent="Você é menor de idade";
     }
     
 }
